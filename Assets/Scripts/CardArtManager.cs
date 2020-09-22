b5515ad7251ebbf7b1d5ca2b44e313d1653c73bf
ThomasH94 <35856664+ThomasH94@users.noreply.github.com>
2020-09-21 22:44:38 -0700

    Initial commit

---
+++ Assets/Scripts/CardArtManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This class will be responsible for updating the card art for each playable card
+/// This will allow the user to use unlocked card art for more customization 
+/// </summary>
+public class CardArtManager : MonoBehaviour
+{
+    public void UpdateCardArtFaces()
+    {
+        
+    }
+    
+    // This method should update all of backs of the cards in the deck and on the field
+    public void UpdateCardArtBacks()
+    {
+        
+    }
+}
